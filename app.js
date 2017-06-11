e176aaf3ef23ae0f7107521159a3a6cc595c0cb7
Nathan Lee <kwnath@gmail.com>
2017-06-11 15:10:03 +0800

    update

---
+++ app.js
@@ -0,0 +1,38 @@
+const AV = require('./utils/av-weapp-min.js');
+
+// Initialization of the app
+AV.init({
+  appId: 'YVQzTyxgE2uuU2u5eLUEFJ9r-gzGzoHsz',
+  appKey: 'rDH9JLD0QGIvO47Di5Yhh9cb',
+});
+
+//app.js
+App({
+  onLaunch: function () {
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+  },
+  getUserInfo:function(cb){
+    var that = this
+    if(this.globalData.userInfo){
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    }else{
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+  },
+  globalData:{
+    userInfo:null
+  }
+})
---
+++ pages/model/form.js
@@ -0,0 +1,4 @@
+// model/form.js
+const AV = require('../utils/av-weapp-min.js');
+class Form extends AV.Object {
+}
